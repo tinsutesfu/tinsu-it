1261e4f23f40efdfe87c1611a3213b7c7bf40dc9
tinsutesfu <yetesfayes@gmail.com>
2024-07-10 14:05:23 -0700

    adjust image uploads

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import React from 'react'
-import Navebar from './component/Navebar';
-import Sidebar from './component/Sidebar';
-import {Routes,Route} from 'react-router-dom';
-import Add from './pages/Add';
-import List from './pages/List';
-import Orders from './pages/Orders';
-import { ToastContainer} from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-  import  './style/admin.css'
-import { Admin } from './pages/Admin';
-
-
+import React from "react";
+import Navebar from "./component/Navebar";
+import Sidebar from "./component/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./style/admin.css";
+import { Admin } from "./pages/Admin";
 
 const App = () => {
-  const url='https://back-it-solution.onrender.com';
+  const url = "https://back-it-solution.onrender.com";
   return (
     <div>
-      <ToastContainer/>
-      <Navebar/>
-      <hr/>
+      <ToastContainer />
+      <Navebar />
+      <hr />
       <div className="app-content">
-        <Sidebar/>
-       
+        <Sidebar />
+
         <Routes>
-          <Route path='/' element={<Admin/>}>
-          <Route index  element={<h1 className='admin'>ADMIN DASHBOARD</h1>}/> 
-          <Route path='add' element={<Add url={url}/>}/>
-          <Route path='list' element={<List url={url}/>}/>
-          <Route path='orders' element={<Orders url={url}/>}/>
+          <Route path="/" element={<Admin />}>
+            <Route index element={<h1 className="admin">ADMIN DASHBOARD</h1>} />
+            <Route path="add" element={<Add url={url} />} />
+            <Route path="list" element={<List url={url} />} />
+            <Route path="orders" element={<Orders url={url} />} />
           </Route>
-        </Routes> 
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,57 +1,62 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import '../style/list.css';
-import axios, { Axios } from 'axios';
-import { toast } from 'react-toastify';
-const List = ({url}) => {
-  const [list,setlist]=useState([]);
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "../style/list.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+const List = ({ url }) => {
+  const [list, setlist] = useState([]);
 
-  const fetchlist=async()=>{
-    const response=await axios.get(`${url}/api/it/list`);
-    
+  const fetchlist = async () => {
+    const response = await axios.get(`${url}/api/it/list`);
+
     if (response.data.success) {
-      setlist(response.data.data)
-    }else{
-      toast.error('error');
+      setlist(response.data.data);
+    } else {
+      toast.error("error");
     }
-  }
+  };
 
-  const removeit=async (itId)=>{
-const response=await axios.post(`${url}/api/it/remove`,{id:itId})
- await fetchlist();
- if (response.data.success) {
-  toast.success(response.data.message)
- }else{
-  toast.error('error');
-}
-  }
-  useEffect(()=>{
-    fetchlist()
-  },[])
+  const removeit = async (itId) => {
+    const response = await axios.post(`${url}/api/it/remove`, { id: itId });
+    await fetchlist();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("error");
+    }
+  };
+  useEffect(() => {
+    fetchlist();
+  }, []);
   return (
-    <div className='list add flex-col'>
+    <div className="list add flex-col">
       <p>all brand list</p>
       <div className="list-table">
         <div className="list-table-format title">
-          <b>image</b><b>name</b><b>ratingstars</b><b>ratingcount</b><b>pricecents</b><b>action</b>
+          <b>image</b>
+          <b>name</b>
+          <b>ratingstars</b>
+          <b>ratingcount</b>
+          <b>pricecents</b>
+          <b>action</b>
         </div>
-        {
-          list.map((item,index)=>{
-            return(
-              <div key={index} className="list-table-format">
-               <img src={`/uploads/${item.image}`}/>
-               <p className='name'>{item.name}</p>
-               <p className='para'>{item.ratingstars}</p>
-               <p className='para'>{item.ratingcount}</p>
-               <p className='para'>${(item.priceCents)/100}</p>
-               <p className='cursor' onClick={()=>removeit(item._id)}>x</p>
-              </div>
-            )
-          })
-        }
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} />
+              <p className="name">{item.name}</p>
+              <p className="para">{item.ratingstars}</p>
+              <p className="para">{item.ratingcount}</p>
+              <p className="para">${item.priceCents / 100}</p>
+              <p className="cursor" onClick={() => removeit(item._id)}>
+                x
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
